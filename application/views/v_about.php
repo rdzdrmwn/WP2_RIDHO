26e875440d3d4630c99837906bde8c255387b690
rdzdrmwn <ridhodarmawan495@gmail.com>
2024-06-21 02:28:31 +0700

    Pustaka Booking

---
+++ application/views/v_about.php
@@ -0,0 +1,26 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan: Saifulloh </li>
+        <li>Nama Belakang: Yusuf</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li>Taman Kebalen ,Blok F1 No 41</li>
+    </ul>
+    <h4>No Telepon</h4>
+    <ul type="none">
+        <li>085717758849</li>
+    </ul>
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Kebumen</li>
+    </ul>
+    <h4>Olahraga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Futsal</li>
+    </ul>
+</section>
